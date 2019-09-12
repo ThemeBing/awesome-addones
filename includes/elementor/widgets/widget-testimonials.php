62cae246235e83f24b6c1f2605b0c19df5cc1bd6
Farhan Khan <farhan01617@gmail.com>
2019-09-12 15:18:08 +0600

    updatre

--- includes/elementor/widgets/widget-testimonials.php
+++ includes/elementor/widgets/widget-testimonials.php
@@ -45,50 +45,20 @@ class megaaddons_Widget_Testimonials extends Widget_Base {
             ],
          ]
       );
-      
-      $this->add_control(
-         'title',
-         [
-            'label' => __( 'Title', 'megaaddons' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __( 'Users love us', 'megaaddons' )
-            
-         ]
-      );
-      
-      $this->add_control(
-         'sub_title',
-         [
-            'label' => __( 'Sub Title', 'megaaddons' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __( 'Lorem ipsum dummy text are used here so replace your app data', 'megaaddons' ),
-            'condition' => ['style' => 'style1']
-            
-         ]
-      );
-      
-      $this->add_control(
-         'testi_image',
-         [
-            'label' => __( 'Image', 'megaaddons' ),
-            'type' => \Elementor\Controls_Manager::MEDIA,
-            'condition' => ['style' => 'style1']
-            
-         ]
-      );
+
 
       $repeater = new \Elementor\Repeater();
 
-      $repeater->add_control(
-         'image',
-         [
-            'label' => __( 'Choose Photo', 'megaaddons' ),
-            'type' => \Elementor\Controls_Manager::MEDIA,
-            'default' => [
-               'url' => \Elementor\Utils::get_placeholder_image_src()
-            ],
-         ]
-      );
+      // $repeater->add_control(
+      //    'image',
+      //    [
+      //       'label' => __( 'Choose Photo', 'megaaddons' ),
+      //       'type' => \Elementor\Controls_Manager::MEDIA,
+      //       'default' => [
+      //          'url' => \Elementor\Utils::get_placeholder_image_src()
+      //       ],
+      //    ]
+      // );
       
       $repeater->add_control(
          'name',
@@ -138,78 +108,70 @@ class megaaddons_Widget_Testimonials extends Widget_Base {
 
       <?php if ( $settings['style'] == 'style1' ){ ?>
 
-      <!-- testimonial-area -->
-      <section class="testimonial-area s-testimonial-bg">
-          <div class="container">
-              <div class="row">
-                  <div class="col-xl-5 col-lg-6">
-                      <div class="testimonial-img">
-                          <img src="<?php echo esc_url( $settings['testi_image']['url'] ); ?>" alt="quote">
-                      </div>
-                  </div>
-                  <div class="col-xl-7 col-lg-6">
-                      <div class="section-title s-section-title white-title border-none mb-70">
-                          <h2><?php echo esc_html( $settings['title'] ); ?></h2>
-                          <p><?php echo esc_html( $settings['sub_title'] ); ?></p>
-                      </div>
-                      <div class="s-testimonial-active">
-                          <?php foreach (  $settings['testimonial_list'] as $testimonial_single ): ?>
-                          <div class="s-single-testimonial">
-                              <div class="s-testi-content mb-40">
-                                  <p><?php echo esc_html($testimonial_single['testimonial']); ?></p>
-                              </div>
-                              <div class="s-testi-avatar">
-                                  <h5><?php echo esc_html($testimonial_single['name']); ?></h5>
-                                  <span><?php echo esc_html($testimonial_single['designation']); ?></span>
-                              </div>
-                          </div>
-                          <?php endforeach; ?>
-                      </div>
+      <div class="testimonial-active">
+        <?php foreach (  $settings['testimonial_list'] as $testimonial_single ): ?>
+          <div class="single-testimonial text-center">
+              <div class="testimonial-icon mb-25">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/quote_icon.png" alt="quote">
+              </div>
+              <div class="testimonial-content">
+                  <h5><?php echo esc_html($testimonial_single['testimonial']); ?></h5>
+                  <div class="testi-avatar">
+                      <h6><?php echo esc_html($testimonial_single['name']); ?></h6>
+                      <span><?php echo esc_html($testimonial_single['designation']); ?></span>
                   </div>
               </div>
           </div>
-      </section>
-      <!-- testimonial-area-end -->
+        <?php endforeach; ?>
+      </div>
       
       <?php } elseif( $settings['style'] == 'style2' ){ ?>
-
-      <!-- testimonial-area -->
-      <section class="testimonial-area">
-          <div class="container">
-              <div class="row justify-content-between">
-                  <div class="col-xl-3 col-lg-4 d-none d-md-block">
-                      <div class="testimonial-quote">
-                          <img src="<?php echo get_template_directory_uri() ?>/img/quote_icon.png" alt="quote">
-                      </div>
+        
+      <div class="row t-testimonial-active">
+        <?php foreach (  $settings['testimonial_list'] as $testimonial_single ): ?>
+          <div class="col-xl-4">
+              <div class="t-single-testimonial text-center">
+                  <div class="t-testimonial-img mb-30">
+                      <img src="<?php echo get_template_directory_uri() ?>/images/testimonial_img01.png" alt="icon">
                   </div>
-                  <div class="col-xl-8 col-lg-8">
-                      <div class="section-title mb-80">
-                          <h2><?php echo esc_html( $settings['title'] ); ?></h2>
-                      </div>
-                      <div class="testimonial-active">
-                           <?php foreach (  $settings['testimonial_list'] as $testimonial_single ): ?>
-                           <div class="single-testimonial">
-                              <div class="testi-content">
-                                  <p><?php echo esc_html($testimonial_single['testimonial']); ?></p>
-                              </div>
-                              <div class="testi-avatar">
-                                  <div class="t-avatar-img">
-                                      <img src="<?php echo esc_url($testimonial_single['image']['url']); ?>" alt="img">
-                                  </div>
-                                  <div class="t-avatar-info">
-                                      <h5><?php echo esc_html($testimonial_single['name']); ?></h5>
-                                      <p><?php echo esc_html($testimonial_single['designation']); ?></p>
-                                  </div>
-                              </div>
-                           </div>
-                           <?php endforeach; ?>
+                  <div class="t-testimonial-content">
+                      <h5><?php echo esc_html($testimonial_single['testimonial']); ?></h5>
+                      <div class="testi-avatar">
+                          <h6><?php echo esc_html($testimonial_single['name']); ?></h6>
+                          <span><?php echo esc_html($testimonial_single['designation']); ?></span>
                       </div>
                   </div>
               </div>
           </div>
-      </section>
-      <!-- testimonial-area-end -->
+        <?php endforeach; ?>
+      </div>
+      
+      <?php } elseif( $settings['style'] == 'style3' ){ ?>
+
+      <div class="s-testimonial-active">
+          <?php foreach (  $settings['testimonial_list'] as $testimonial_single ): ?>
+          <div class="single-testimonial s-single-testimonial text-center">
+              <div class="testimonial-icon mb-25">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/quote02.png" alt="icon">
+              </div>
+              <div class="testimonial-content">
+                  <h5>“In promotion and of advertising, a testimonial or show consists person's written or spoken statement extoll product
+                  "testimonial "</h5>
+                  <div class="s-testi-avatar">
+                      <div class="testi-avatar-img">
+                          <img src="img/images/testi_avatar_img01.png" alt="img">
+                      </div>
+                      <div class="testi-avatar-info">
+                          <h6><?php echo esc_html($testimonial_single['name']); ?> _ <span><?php echo esc_html($testimonial_single['designation']); ?></span></h6>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <?php endforeach; ?>
+      </div>
+
       <?php } ?>
+
    <?php } 
  
 }
