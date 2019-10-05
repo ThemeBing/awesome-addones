221e39d987ec41d064c3bfa6d83a74e5e5766473
Farhan Khan <farhan01617@gmail.com>
2019-10-05 10:32:27 +0600

    Update widget-blog.php

--- includes/elementor/widgets/widget-blog.php
+++ includes/elementor/widgets/widget-blog.php
@@ -69,12 +69,22 @@ class megaaddons_Widget_Blog extends Widget_Base {
                 <div class="single-blog-post mb-30">
                     <div class="b-post-thumb">
                       <a href="<?php the_permalink() ?>"><img src="<?php echo get_the_post_thumbnail_url( get_the_ID(),'saascloud-404x302'); ?>" alt="<?php the_title() ?>"></a>
+                      <div class="category">
+                        <?php 
+                        $categories = get_the_category();
+                        if ( ! empty( $categories ) ) {
+                            echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+                        } ?>
+                      </div>
                     </div>
                     <div class="blog-content">
-                        <span><?php the_date(); ?></span>
-                        <h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
-                        <p><?php echo wp_trim_words( get_the_content(), 7, '.' ); ?></p>
-                        <a href="<?php the_permalink(); ?>"><?php echo esc_html__( 'Read More', 'megaaddons' ); ?> <i class="fa fa-plus"></i></a>
+                      <ul class="list-inline">
+                        <li class="list-inline-item"><i class="fa fa-user"></i><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php echo esc_html__( 'By','zaaple' ); ?> <?php the_author(); ?></a></li>
+                        <li class="list-inline-item"><i class="fa fa-calendar"></i><?php the_date(); ?></li>
+                      </ul>
+                      <h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+                      <p><?php echo wp_trim_words( get_the_content(), 7, '.' ); ?></p>
+                      <a href="<?php the_permalink(); ?>"><?php echo esc_html__( 'Read More', 'megaaddons' ); ?> <i class="fa fa-long-arrow-right"></i></a>
                     </div>
                 </div>
               </div>
